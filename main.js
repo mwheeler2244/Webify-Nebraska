c73c9a40c1951fde5d3e36c2d5a5fec3b8191f97
Michael  Wheeler <mwheeler2244@yahoo.com>
2024-10-04 15:54:29 -0500

    update subpages

--- main.js
+++ main.js
@@ -1,44 +1,29 @@
-
-
 const navLinks = document.querySelector(".nav__links");
-const mobileOpen = document.querySelector(".mobile__open")
-const mobileClose = document.querySelector(".mobile__close")
-const links = document.querySelectorAll("li")
+const mobileOpen = document.querySelector(".mobile__open");
+const mobileClose = document.querySelector(".mobile__close");
+const links = document.querySelectorAll("li");
 
+mobileOpen.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
-
-
-mobileOpen.addEventListener('click', ()=>{
-    navLinks.classList.toggle("active")
-})
-
-mobileClose.addEventListener('click', ()=>{
-  navLinks.classList.toggle('active')
-})
+mobileClose.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
 links.forEach((link) => {
-  link.addEventListener('click', () =>{
-    navLinks.classList.toggle('active')
-  })
-})
+  link.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
 
+window.addEventListener("scroll", function () {
+  let nav = document.querySelector("nav");
+  let heroHeight = document.getElementById("hero").offsetHeight;
 
-
-
-
-
-
-
-
- window.addEventListener("scroll", function () {
-     let nav = document.querySelector("nav");
-     let heroHeight = document.querySelector(".hero").offsetHeight;
-
-     if (window.scrollY > heroHeight) {
-       nav.classList.add("sticky-nav");
-     } else {
-       nav.classList.remove("sticky-nav");
-     }
-   });
-
-
+  if (window.scrollY > heroHeight) {
+    nav.classList.add("sticky-nav");
+  } else {
+    nav.classList.remove("sticky-nav");
+  }
+});
